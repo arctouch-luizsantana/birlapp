2f01c628a76a7aa0c1d7a6dbc132a7746d638e6e
Luiz Santana <luiz.santana@arctouch.com>
2016-10-26 23:24:06 -0200

    Step 03

--- app/src/main/java/br/unisul/birlapp/MainActivity.java
+++ app/src/main/java/br/unisul/birlapp/MainActivity.java
@@ -4,20 +4,28 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int nivel = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView titulo = (TextView) findViewById(R.id.titulo);
         Button botao = (Button) findViewById(R.id.button);
+        ImageView imagem = (ImageView) findViewById(R.id.imageView);
+
         botao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Ainda não implementado", Toast.LENGTH_SHORT).show();
+                nivel++;
+                Toast.makeText(getApplicationContext(), "Nivel atual: " + nivel, Toast.LENGTH_SHORT).show();
             }
         });
     }
